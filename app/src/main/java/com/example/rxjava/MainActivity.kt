ef72b9e40c6cbd3fd9c6ea097bde20d6bb6b170d
Rfed0r0v <rf.rf944@gmail.com>
2022-12-18 20:56:41 +0300

    2

--- app/src/main/java/com/example/rxjava/MainActivity.kt
+++ app/src/main/java/com/example/rxjava/MainActivity.kt
@@ -12,7 +12,6 @@ import io.reactivex.rxjava3.schedulers.Schedulers
 import timber.log.Timber
 import timber.log.Timber.DebugTree
 
-
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,9 +31,7 @@ class MainActivity : AppCompatActivity() {
             }.subscribe(
                 {
                     Timber.d("ObserverOut: $it")
-                },
-                {},
-                {}
+                }, {}, {}
             )
 
 
@@ -45,21 +42,21 @@ class MainActivity : AppCompatActivity() {
             }
 
             override fun onNext(t: Int) {
-                Timber.d( "onNext ${t * 10}")
+                Timber.d("onNext ${t * 10}")
             }
 
             override fun onError(e: Throwable) {
-                Timber.d( "onError $e")
+                Timber.d("onError $e")
             }
 
             override fun onComplete() {
-                Timber.d( "onComplete ")
+                Timber.d("onComplete ")
             }
         }
         val subscriber2 = object : Observer<Int> {
 
             override fun onSubscribe(d: Disposable) {
-                Timber.d( "onSubscribe 2 ")
+                Timber.d("onSubscribe 2 ")
             }
 
             override fun onNext(t: Int) {
@@ -76,66 +73,41 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
-//        Потоки
-//        doOnNext вся труба с объектами выполняется в main потоке
-//        Thread.currentThread().name- дает название общего потока
-//        Как переключить поток в RX:subscribeOn
-//        Есть определенные константы Schedulers
-//         Константа Schedulers.io()- для работы с input и output
-//         Константа Schedulers.newThread()- создает отдельный thread. Если его поместить
-//         до doOnNext- создается отдельный поток, в котором вызываются Item
-//        Константа AndroidSchedulers.mainThread()- переключение на главный поток до
-//         момента пока либо не зхакончится код, либо не встретится observeOn
-//         в случае ниже, создался один поток, он идт до того момента, пока не увидил
-//         observeOn - и создается новый поток
-//                      .subscribeOn(Schedulers.newThread() -
-//                    .observeOn(Schedulers.newThread())
-
+// -----------------------------------------------------------------------
+// Переключение потоков
         someObservable
             .subscribeOn(Schedulers.newThread())
             .doOnNext {
                 Timber.d("doOnNext ${Thread.currentThread().name}")
             }
-//            .subscribeOn(Schedulers.io())
+            .subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
             .doOnNext {
-                Timber.d( "doOnNext ${Thread.currentThread().name}")
+                Timber.d("doOnNext ${Thread.currentThread().name}")
             }
             .subscribe(
                 {
-                    Timber.d( "onNext $it")
+                    Timber.d("onNext $it")
                 },
                 {},
                 {}
             )
 
-//        Flowable - Observer который может работать с потоком больших данных и может его
-//         регулировать с помощью технологии Strategy
-//        Методы .onBackpressureBuffer() - позволяет буферизирует до определенного кол-ва элементов
-//         позволяет подключить определенную стратегию обработки данных. Если подписчики не успеют
-//         обработать данные до того момента когда  Flowable выкинет данные из буфера заданного объема
-//        то тогдабудеет вызван OnError, потомучто subscriber не успеет обработать данные
-//
-//        Мы можем выбирать - буферизировать данные,или отбрасываем самые старые события, можем откидывать данные
-
+//------------------------------------------------------------------------------------
         Flowable
-            .just(1, 1, 1, 4, 5, 6, 7, 8, 9, 10)
+            .just(0, 0, 2, 4, 5, 6, 7, 8, 9, 10)
             .onBackpressureBuffer(4)
             .subscribe({
-                Timber.d("onNext $it"
+                Timber.d(
+                    "onNext $it"
                 )
             }, {}, {}
             )
-
-
-        // Single - Observable, который вызывается единожды, получает данные и умирает
-        // livedata сетится только на главном потоке
+// -----------------------------------------------------------------------------------
         Single.just(1)
             .subscribe({
 
-            }, {
-
-            })
+            }, { })
 
 
     }
